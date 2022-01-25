6539829a0947671927c8e974ed78ca2d576da870
MrWalshyType2 <mwalsh@qa.com>
2022-01-25 15:52:09 +0000

    Added startup listener for initialising db with some data

---
+++ src/main/java/com/qa/user_app/configuration/ApplicationStartupListener.java
@@ -0,0 +1,34 @@
+package com.qa.user_app.configuration;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+
+import com.qa.user_app.data.entity.User;
+import com.qa.user_app.data.repository.UserRepository;
+
+@Configuration
+public class ApplicationStartupListener implements ApplicationListener<ApplicationReadyEvent> {
+
+	private UserRepository userRepository;
+	
+	@Autowired
+	public ApplicationStartupListener(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	// The application has booted and its components are ready to server
+	// content when this method fires
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		userRepository.saveAll(List.of(
+				new User("Fred", "Daly", 32),
+				new User("Sarah", "Daly", 36),
+				new User("Bob", "Sir", 27)
+		));
+	}
+
+}
